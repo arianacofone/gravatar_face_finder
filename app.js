d3f5bc4568e28d3d75eabfc9e47cfb979817885b
arianadziedzic <ariana.dziedzic@gmail.com>
2017-02-26 16:09:28 -0500

    functioning challenge

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('connected');
+
+
+
+let md5 = function(value) {
+  let modifiedValue = value.trim().toLowerCase();
+  return CryptoJS.MD5(value).toString();
+}
+
+function request() {
+  let email = document.querySelector('.email').value;
+  let hash = md5(email);
+  let url = `http://www.gravatar.com/avatar/${hash}`;
+
+  let image = document.querySelector('.image').src = url;
+
+  document.querySelector('.image').innerHTML = url;
+  return (false);
+}
+
+// http://jsfiddle.net/33HMj/
